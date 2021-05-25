3696849fcabfa8e3e3243686c088c20da1a74877
Austin <sicardaustin97@gmail.com>
2021-05-25 12:08:27 +0000

    Completion of exercise2, loads the first and second store from the database and renames the first store to Ottawa

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+# Update and change name of Store1
+@store1.update(name: "Ottawa")
